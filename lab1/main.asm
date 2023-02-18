e0f59195c1fd3768f50da746899080a58303ba16
reddiridabl666 <reddiridabl666@gmail.com>
2023-02-18 08:21:36 +0300

    Kinda done lab1, but the main part is writing the report

---
+++ lab1/main.asm
@@ -0,0 +1,56 @@
+bits 64;
+
+section .data              ; сегмент инициализированных переменных
+    ExitMsg db "Press Enter to Exit",10  ; выводимое сообщение
+    lenExit equ $-ExitMsg
+
+    A dw -30
+    B dw 21
+
+    val1 db 255
+    chart dw 256
+    lue3 dw -128
+    v5 db 10h
+     db 100101B
+    beta db 23, 23h, 0ch
+    sdk db "Hello", 10
+    min dw -32767
+    valar times 5 db 8
+
+    val dw 25
+    val2 dd -35
+    name db "Леонид Leonid"
+
+section .bss               ; сегмент неинициализированных переменных
+    InBuf   resb    10            ; буфер для вводимой строки
+    lenIn   equ     $-InBuf
+
+    X resd 1
+
+    alu resw 10
+    fl resb 5
+    
+section .text         ; сегмент кода
+global  _start
+_start:
+    mov EAX,[A] ; загрузить число A в регистр EAX
+    add EAX,5   ; сложить EAX и 5, результат в EAX
+    sub EAX,[B] ; вычесть число B, результат в EAX
+    mov [X],EAX ; сохранить результат в памяти
+    
+    ; write
+    mov     rax, 1        ; системная функция 1 (write)
+    mov     rdi, 1        ; дескриптор файла stdout=1
+    mov     rsi, ExitMsg  ; адрес выводимой строки
+    mov     rdx, lenExit  ; длина строки
+    syscall               ; вызов системной функции
+    ; read
+    mov     rax, 0        ; системная функция 0 (read)
+    mov     rdi, 0        ; дескриптор файла stdin=0
+    mov     rsi, InBuf    ; адрес вводимой строки
+    mov     rdx, lenIn    ; длина строки
+    syscall               ; вызов системной функции
+    ; exit
+    mov     rax, 60       ; системная функция 60 (exit)
+    xor     rdi, rdi      ; return code 0    
+    syscall               ; вызов системной функции
