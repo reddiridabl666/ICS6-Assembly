5fd16a3378c424d615ad8ab3e32d673c20163de9
reddiridabl666 <reddiridabl666@gmail.com>
2023-02-21 10:49:56 +0300

    Words to be swapped are found

--- lab5/src/main.cpp
+++ lab5/src/main.cpp
@@ -15,27 +15,30 @@ int main() {
     std::string input;
     std::getline(std::cin, input);
 
-    std::cout << "Input numbers of words you want to swap" << std::endl;
+    // std::cout << "Input numbers of words you want to swap" << std::endl;
 
     size_t first, second;
-    std::cin >> first;
-    if (std::cin.bad()) {
-        std::cout << kInvalidInput << std::endl;
-        return -1;
-    }
+    first = 1;
+    second = 3;
 
-    std::cin >> second;
-    if (std::cin.bad()) {
-        std::cout << kInvalidInput << std::endl;
-        return -1;
-    }
+    // std::cin >> first;
+    // if (std::cin.bad()) {
+    //     std::cout << kInvalidInput << std::endl;
+    //     return -1;
+    // }
 
-    if (input.size() > kMaxLength) {
-        std::cout << "Input should be <= 255 characters long" << std::endl;
-        return -1;
-    }
+    // std::cin >> second;
+    // if (std::cin.bad()) {
+    //     std::cout << kInvalidInput << std::endl;
+    //     return -1;
+    // }
+
+    // if (input.size() > kMaxLength) {
+    //     std::cout << "Input should be <= 255 characters long" << std::endl;
+    //     return -1;
+    // }
     
-    input += '\n';
+    input += ' ';
     /* std::cout << */ swap_words(input.c_str(), input.size(), first, second) /* << std::endl */;
 
     return 0;
--- lab5/src/swap_words.asm
+++ lab5/src/swap_words.asm
@@ -1,15 +1,24 @@
 bits 64;
 
+%define STDOUT 1
+%define WRITE 1
+
 %define MAX_LEN 255
 
 section .data
-    msg db "Asm successfully called",10
-    msg_len equ $-msg
-
-    input times MAX_LEN db 0
+    enter_ db 10
 
 section .bss
     input_len resq 1
+    first_len resq 1
+    second_len resq 1
+
+    output resb MAX_LEN
+    first_word resb MAX_LEN
+    second_word resb MAX_LEN
+
+    input_start resq 1
+
     first resq 1
     second resq 1
 
@@ -17,19 +26,115 @@ section .text
 global swap_words
 
 swap_words:
+    mov [input_start], rdi
     mov [input_len], rsi
     mov [first], rdx
-    mov [second], r10
+    mov [second], rcx
 
     mov rcx, [input_len]
-    mov rsi, rdi
-    mov rdi, input
+    mov rsi, [input_start]
+    mov rax, rsi
+    mov rdi, output
+    xor rdx, rdx
+
+find_space:
+    dec rcx
+    cmp rcx, 0
+    jle end
+
+    inc rsi
+
+    cmp byte[rsi], ' '
+    jne find_space
+
+    inc rdx
+    inc rsi
+
+    cmp rdx, [first]
+    je process_first
+
+    cmp rdx, [second]
+    je process_second
+
+    mov rax, rsi
+    jmp find_space
+
+process_first:
+    mov rdi, first_word
+    mov rbx, first_len
+    call process_word
+    jmp find_space
+
+process_second:
+    mov rdi, second_word
+    mov rbx, second_len
+    call process_word
+    jmp form_result
+
+form_result:
+    mov rsi, input_start
+    mov rdi, output
+    mov rcx, input_len
+    mov rdx, 1
+
+result_loop:
+    cmp rdx, [first]
+    je add_second
+
+    cmp rdx, [second]
+    je add_first
+    
+    movsb
+    cmp byte[rsi], ' '
+    jne next
+    
+    inc rdx
+
+next:   
+    dec rcx
+    jmp result_loop
+
+add_first:
+
+add_second:
+
+end:
+    mov rax, WRITE
+    mov rdi, STDOUT
+    mov rsi, first_word
+    mov rdx, [first_len]
+    syscall
+
+    mov rax, WRITE
+    mov rdi, STDOUT
+    mov rsi, enter_
+    mov rdx, 1
+    syscall
+
+    mov rax, WRITE
+    mov rdi, STDOUT
+    mov rsi, second_word
+    mov rdx, [second_len]
+    syscall
+
+    mov rax, WRITE
+    mov rdi, STDOUT
+    mov rsi, enter_
+    mov rdx, 1
+    syscall
+    ret
+
+process_word:
+    push rcx
+    mov rcx, rsi
+    sub rcx, rax
+    mov [rbx], rcx
+    push rsi
+    mov rsi, rax
+
     cld
     rep movsb
 
-    mov rdx, input_len
-    mov rsi, input
-    mov rax, 1
-    mov rdi, 1
-    syscall
+    pop rsi
+    pop rcx
     ret
