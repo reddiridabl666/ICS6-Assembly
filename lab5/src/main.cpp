b41f6fd116ab8734d7cf7c254ee26e0b877ee946
reddiridabl666 <reddiridabl666@gmail.com>
2023-02-21 13:25:55 +0300

    lab5 almost working

--- lab5/src/main.cpp
+++ lab5/src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <string_view>
+#include <ranges>
+#include <vector>
 
 static constexpr size_t kMaxLength = 255;
 static constexpr std::string_view kInvalidInput = "Invalid input";
@@ -9,37 +11,73 @@ extern "C" {
     const char* swap_words(const char* input, size_t size, size_t first, size_t second);
 }
 
+std::vector<size_t> get_numbers(const std::string& str) {
+    std::vector<size_t> res;
+    size_t num = 0;
+    bool found = false;
+
+    for (char c : str) {
+        if (c > '0' && c < '9') {
+            num = num * 10 + (c - '0');
+            found = true;
+            continue;
+        }
+
+        if (c == ' ') {
+            res.push_back(num);
+            num = 0;
+            found = false;
+            continue;
+        }
+
+        throw std::runtime_error("Invalid input");
+    }
+
+    if (found) {
+        res.push_back(num);
+    }
+    return res;
+}
+
 int main() {
-    std::cout << "Input text with <= 255 characters" << std::endl;
+    std::cout << "Input line of text with <= 255 characters" << std::endl;
 
     std::string input;
     std::getline(std::cin, input);
 
-    // std::cout << "Input numbers of words you want to swap" << std::endl;
+    if (input.size() > kMaxLength) {
+        std::cout << "Input should be <= 255 characters long" << std::endl;
+        return -1;
+    }
 
-    size_t first, second;
-    first = 1;
-    second = 3;
+    std::string input_numbers;
 
-    // std::cin >> first;
-    // if (std::cin.bad()) {
-    //     std::cout << kInvalidInput << std::endl;
-    //     return -1;
-    // }
+    do {
+        std::cout << "\nInput numbers of words you want to swap or 'end' to exit" << std::endl;
+        std::getline(std::cin, input_numbers);
 
-    // std::cin >> second;
-    // if (std::cin.bad()) {
-    //     std::cout << kInvalidInput << std::endl;
-    //     return -1;
-    // }
+        if (input_numbers == "end") {
+            break;
+        }
 
-    // if (input.size() > kMaxLength) {
-    //     std::cout << "Input should be <= 255 characters long" << std::endl;
-    //     return -1;
-    // }
+        std::vector<size_t> numbers;
     
-    input += ' ';
-    /* std::cout << */ swap_words(input.c_str(), input.size(), first, second) /* << std::endl */;
+        try {
+            numbers = get_numbers(input_numbers);
+        } catch(std::runtime_error& e) {
+            std::cout << e.what() << std::endl;
+            return -1;
+        }
+
+        if (numbers.size() != 2) {
+            std::cout << "You should input 2 numbers seperated by one space" << std::endl;
+            continue;
+        }
+
+        input += ' ';
+        input = swap_words(input.c_str(), input.size(), numbers[0], numbers[1]);
+        std::cout << input << std::endl;
+    } while (true);
 
     return 0;
 }
--- lab5/src/swap_words.asm
+++ lab5/src/swap_words.asm
@@ -2,18 +2,20 @@ bits 64;
 
 %define STDOUT 1
 %define WRITE 1
+%define EXIT 60
 
 %define MAX_LEN 255
 
 section .data
-    enter_ db 10
+    err_msg db "Invalid word position",10
+    err_len equ $-err_msg
 
 section .bss
     input_len resq 1
     first_len resq 1
     second_len resq 1
 
-    output resb MAX_LEN
+    output resb MAX_LEN + 1
     first_word resb MAX_LEN
     second_word resb MAX_LEN
 
@@ -28,6 +30,7 @@ global swap_words
 swap_words:
     mov [input_start], rdi
     mov [input_len], rsi
+
     mov [first], rdx
     mov [second], rcx
 
@@ -40,7 +43,7 @@ swap_words:
 find_space:
     dec rcx
     cmp rcx, 0
-    jle end
+    jle error
 
     inc rsi
 
@@ -71,63 +74,11 @@ process_second:
     call process_word
     jmp form_result
 
-form_result:
-    mov rsi, input_start
-    mov rdi, output
-    mov rcx, input_len
-    mov rdx, 1
-
-result_loop:
-    cmp rdx, [first]
-    je add_second
-
-    cmp rdx, [second]
-    je add_first
-    
-    movsb
-    cmp byte[rsi], ' '
-    jne next
-    
-    inc rdx
-
-next:   
-    dec rcx
-    jmp result_loop
-
-add_first:
-
-add_second:
-
-end:
-    mov rax, WRITE
-    mov rdi, STDOUT
-    mov rsi, first_word
-    mov rdx, [first_len]
-    syscall
-
-    mov rax, WRITE
-    mov rdi, STDOUT
-    mov rsi, enter_
-    mov rdx, 1
-    syscall
-
-    mov rax, WRITE
-    mov rdi, STDOUT
-    mov rsi, second_word
-    mov rdx, [second_len]
-    syscall
-
-    mov rax, WRITE
-    mov rdi, STDOUT
-    mov rsi, enter_
-    mov rdx, 1
-    syscall
-    ret
-
 process_word:
     push rcx
     mov rcx, rsi
     sub rcx, rax
+    dec rcx
     mov [rbx], rcx
     push rsi
     mov rsi, rax
@@ -138,3 +89,82 @@ process_word:
     pop rsi
     pop rcx
     ret
+
+form_result:
+    mov rsi, [input_start]
+    mov rdi, output
+    mov rcx, [input_len]
+    mov rdx, 1
+result_loop:
+    cmp rcx, 0
+    jle end
+
+    cmp rdx, [first]
+    je add_second
+
+    cmp rdx, [second]
+    je add_first
+    
+    cld
+    movsb
+
+    cmp byte[rsi], ' '
+    jne next
+    
+    movsb
+    inc rdx
+
+next:   
+    dec rcx
+    jmp result_loop
+
+add_first:
+    mov rax, [second_len]
+    mov rbx, first_word
+    mov r10, [first_len]
+
+    call add_word
+    jmp result_loop
+    
+
+add_second:
+    mov rax, [first_len]
+    mov rbx, second_word
+    mov r10, [second_len]
+
+    call add_word
+    jmp result_loop
+
+add_word:
+    add rsi, rax
+    sub rcx, rax
+    push rcx
+    push rsi
+    mov rsi, rbx
+    mov rcx, r10
+
+    cld
+    rep movsb
+
+    pop rsi
+    pop rcx
+
+    inc rdx
+    ret
+
+end:
+    mov rax, [input_len]
+    mov byte[output + rax - 1], 0
+    mov rax, output
+    ret
+
+error:
+    mov rax, WRITE
+    mov rdi, STDOUT
+    mov rsi, err_msg
+    mov rdx, err_len
+    syscall
+
+    mov rdx, -1
+    mov rax, EXIT
+    syscall
